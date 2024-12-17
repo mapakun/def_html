850af661a596798ca2ceaed2498639f327610f6e
mapakun <hayoko9@gmail.com>
2024-12-18 01:30:15 +0900

    update index page

---
+++ js/pagejs/indexpage.js
@@ -0,0 +1,7 @@
+document.getElementById('indexTest').addEventListener('click', () => {
+    console.log('이벤트 발생테스트');
+});
+
+document.getElementById('apiTest').addEventListener('click', () => {
+
+});
