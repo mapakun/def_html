210e378d23db228a9348620fe78df86bd78a1d99
mapakun <hayoko9@gmail.com>
2024-12-21 22:36:50 +0900

    new func dbapitest

--- js/pagejs/indexpage.js
+++ js/pagejs/indexpage.js
@@ -18,3 +18,27 @@ function fnCallback(svcId) {
         }
     }
 };
+
+const req = {
+    commonInDTO : {
+        test: "hihi"
+    }
+};
+
+document.getElementById('dbCon').addEventListener('click', () => {
+    commonAjax("test", "POST", "/api/dbtest", req, "onload error", fnCallback2("test"))
+});
+
+function fnCallback2(svcId, data) {
+    return function(svcId, data) {
+        if (svcId === "onload") {
+            console.log("svcId = " + svcId);
+            console.log("onload 성공");
+        }
+        if (svcId === "test") {
+            console.log("svcId = " + svcId);
+            console.log("data = " + JSON.stringify(data));
+        }
+    }
+    console.log(data);
+};
